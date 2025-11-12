d612fa862a2ec5f6aca19d686ad77c049adf3615
paxluporum <paxluporum@gmail.com>
2025-11-12 11:02:16 -0500

    shuffle array function

--- code.js
+++ code.js
@@ -1,3 +1,24 @@
+import { Toolbox } from "./toolbox.js";
 
 let canvas = document.getElementById("myCanvas");
-let pencil = canvas.getContext("2d"); 
+let pencil = canvas.getContext("2d");
+let toolbox = new Toolbox();
+
+
+let myFavoriteLetters = ["a", "b", "c"];
+
+
+
+
+
+// let counts = [0,0,0];
+// for (let i = 0; i < 100; i++) {
+//     //     let randomIndex = getRandomIndex(myFavoriteLetters);
+//     //     counts[randomIndex] = counts[randomIndex] + 1;
+//     // }
+//     console.log(toolbox.getRandomItem(myFavoriteLetters));
+// }
+
+
+let shuffled = toolbox.shuffleArray(myFavoriteLetters);
+    console.log(shuffled);
---
+++ toolbox.js
@@ -0,0 +1,32 @@
+export class Toolbox {
+
+    //gets a random number 0 -> array.length, given an array.
+    getRandomIndex(array) {
+        return Math.floor(
+            Math.random() * array.length
+        );
+    }
+
+    //gets a random item from an array
+    getRandomItem(array) {
+        let randomIndex = this.getRandomIndex(array);
+        return array[randomIndex];
+    }
+
+    shuffleArray(array) {
+        let shuffled = [];
+
+        let howManyTimesToPush = array.length;
+
+        // shuffled.push(array[0]);?????????????
+        for(let i = 0; i < howManyTimesToPush; i++) {
+            let randomIndex = this.getRandomIndex(array);
+            let removed = array.splice(randomIndex, 1) //start position, length
+            shuffled.push(removed[0]);
+        }
+        return shuffled;
+    }
+}
+
+
+

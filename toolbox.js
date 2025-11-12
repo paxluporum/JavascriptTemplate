c2758005077df1833f3802af91525f76cabc9360
paxluporum <paxluporum@gmail.com>
2025-11-12 11:15:39 -0500

    end of class

--- toolbox.js
+++ toolbox.js
@@ -26,6 +26,13 @@ export class Toolbox {
         }
         return shuffled;
     }
+
+    getRandomColor(array) {
+        let randomColor = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];  
+        
+        
+        //this.getRandomColor
+    }
 }
 
 
